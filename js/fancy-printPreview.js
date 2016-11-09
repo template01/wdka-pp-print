ec852e6ab68a32eb042cdb50a01b05d2bf42efcb
valiz user <contact@template01.info>
2016-11-09 18:45:13 +0100

    updates

--- js/fancy-printPreview.js
+++ js/fancy-printPreview.js
@@ -4,28 +4,52 @@ var printPreview = (function() {
         alert(id)
     }
 
+
+    createEmptyPost = function() {
+
+        $("#printpreview").append(`
+      <li id="postid-empty" class="tablePost">
+        <div class="tablePostMeta">
+        </div>
+        <div class="tablePostContent section-default">
+
+          <div class='content'>
+            <h1> Please return to the list and make a selection</h1>
+          </div>
+
+        </div>
+
+      </li>
+    `)
+    }
+
     getPrintPreview = function(showEvent) {
 
         $(document).on('click', showEvent, function() {
-
-            // $(this).toggleClass('showList')
-
-
-
-            $("#rightContent").children().toggleClass('disable')
-            $("#printpreview").toggle()
-            $("#tableWrapper").toggle()
-            $("#printpreview").find('.tablePostMeta').empty()
-                //
-                if($('#printpreview:in-viewport').length>0){
+            if($("#basket").children().length>0){
+              $("#rightContent").children().toggleClass('disable')
+              $("#printpreview").toggle()
+              $("#tableWrapper").toggle()
+              $("#printpreview").find('.tablePostMeta').empty()
+              if ($('#printpreview:in-viewport').length > 0) {
                   $("#printpreview").find('.tablePostMeta').append(`
-                    <div id="printpreviewSelection" class="tablePostMetaItem long"><p>Print Preview: </p></div>
-                    <div class="tablePostMetaItem back"><p><span class="fa fa-long-arrow-left"></span> Back to list<p></div>
-                    `)
-                  $("#basket .basketItem").each(function(){$("#printpreviewSelection p").append($(this).find('p').text()+', ')})
-                }
+                      <div id="printpreviewSelection" class="tablePostMetaItem long"><p>Print Preview: </p></div>
+                      <div class="tablePostMetaItem back"><p><span class="fa fa-long-arrow-left"></span> Back to list<p></div>
+                      `)
+                  $("#basket .basketItem").each(function() {
+                      $("#printpreviewSelection p").append($(this).find('p').text() + ', ')
+                  })
+              }
+            }else{
+              $(showEvent).addClass('makeSelection')
+              window.setTimeout(function(){
+                $(showEvent).removeClass('makeSelection')
+              },600)
+            }
+
 
         })
+
     }
 
     createShowEventElements = function() {
@@ -39,6 +63,7 @@ var printPreview = (function() {
     }
 
     return {
+        createEmptyPost: createEmptyPost,
         createShowEventElements: createShowEventElements,
         addToPrintPreview: addToPrintPreview,
         getPrintPreview: getPrintPreview,
